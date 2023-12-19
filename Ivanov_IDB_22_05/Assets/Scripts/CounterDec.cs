14739ee2046429cfbbef695256af44a982cf6c1d
darkside's adept <ivanov-rr@inbox.ru>
2023-12-19 23:43:27 +0300

    45

--- Ivanov_IDB_22_05/Assets/Scripts/CounterDec.cs
+++ Ivanov_IDB_22_05/Assets/Scripts/CounterDec.cs
@@ -6,19 +6,21 @@ using TMPro;
 
 public class CounterDec : MonoBehaviour
 {
-    public TMP_Text text;
+    [SerializeField] private TMP_Text text;
     public int counter = 100;
 
     private void OnTriggerEnter(Collider other)
     {
         counter--;
+        text.text = $"counter\n  {counter}";//Convert.ToString(counter);
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        text.text = $"counter\n  {counter}";//Convert.ToString(counter);
+    //void Update()
+    //{
+    //    text.text = $"counter\n  {counter}";//Convert.ToString(counter);
 
 
-    }
+    //}
 }
